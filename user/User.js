7abc76922d8b552d620243d9a3a403ca28592021
Bernardo Bechtold Pereira <bernardo.bechtold.pereira@sap.com>
2018-03-20 21:25:59 +0100

    first commit

---
+++ user/User.js
@@ -0,0 +1,9 @@
+var mangoose = require("mongoose");
+var UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User');
---
+++ user/UserController.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+var User = require('./User');
+
+//Creates a new user
+router.post('/', function (req, res) {
+    User.create({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password
+        }, 
+        function (err, user) {
+            if (err) 
+                return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(user);
+        });
+});
+
+// RETURNS ALL THE USERS IN THE DATABASE
+router.get('/', function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) return res.status(500).send("There was a problem finding the users.");
+        res.status(200).send(users);
+    });
+});
+
+module.exports = router;
